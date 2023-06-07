be3c775c5dcb7d54a0dfb0ff40803519d8840d29
Enzo Rossi Daltoé <enzorossidaltoe@hotmail.com.br>
2023-06-07 17:53:10 -0300

    Banner and Header Edits

--- src/Componentes/Banner/Banner.js
+++ src/Componentes/Banner/Banner.js
@@ -5,6 +5,7 @@ function MainBanner() {
         <main className='Main'>
             <section className='Main-Banner'>
                 <h1 className="Main-Banner-Title">Olá</h1>
+                <p className='Main-Banner-Text'>me chamo Enzo, bem vindo ao meu site</p>
                 <img alt='Banner principal' src='/img/banner.jpg' className='Main-Banner-Img'/>
             </section>
         </main>
--- src/Componentes/Header/Header.js
+++ src/Componentes/Header/Header.js
@@ -1,14 +1,15 @@
+
 function Header() {
     return (
         <header className="App-header">
-        <h1 className='Header-Name'>enz0rd.dev</h1>
-        <nav className='Header-Nav'>
-          <a className='Header-Nav-Item' href='#'>Início</a>
-          <a className='Header-Nav-Item' href='#'>Artigos</a>
-          <a className='Header-Nav-Item' href='#'>Linktree</a>
-          <a className='Header-Nav-Item' href='#'>Sobre</a>
-        </nav>
-      </header>
+            <h1 className='Header-Name'>enz0rd.dev</h1>
+            <nav className='Header-Nav'>
+                <a className='Header-Nav-Item' href='#'>Início</a>
+                <a className='Header-Nav-Item' href='#'>Artigos</a>
+                <a className='Header-Nav-Item' href='#'>Linktree</a>
+                <a className='Header-Nav-Item' href='#'>Sobre</a>
+            </nav>
+        </header>
     )
 }
 
