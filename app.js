78310df285ea9a2a4880e28cd2c96091c189ab7f
Enzo Rossi Daltoé <enzorossidaltoe@hotmail.com.br>
2023-06-27 13:30:10 -0300

    MIME Type

--- app.js
+++ app.js
@@ -12,7 +12,15 @@ const port = 3000;
 app.listen(port, () => {
     console.log(`rodando:
     http://localhost:3000/`)
-    app.use(express.static('public'))
+    app.use(express.static('public', { 
+        setHeaders: (res, path, stat) => {
+          if (path.endsWith('.css')) {
+            res.set('Content-Type', 'text/css');
+          } else if (path.endsWith('.js')) {
+            res.set('Content-Type', 'text/javascript');
+          }
+        }
+      }));
     app.use(function(req, res, next) {
         res.status(404)
 
