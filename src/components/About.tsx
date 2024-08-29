592ed838e69a5ea3ffac327c93a0fb95ad55bdb8
Enzo Rossi Daltoé <enzorossidaltoe@hotmail.com.br>
2024-08-29 13:30:33 -0300

    added email address

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,11 +1,38 @@
+import { useState } from 'react';
+import { Button } from './ui/button';
 import imageBackgroundAbout from '/images/image-background-about.jpg';
+import {MailIcon, CopyIcon} from 'lucide-react'
 
 const About = () => {
+    const [isClicked, setClicked] = useState(false);
+
+    const handleClickEmail = () => {
+        navigator.clipboard.writeText('enzorossidaltoe@hotmail.com.br')
+        setClicked(true)
+        setTimeout(() => setClicked(false), 3000)
+    }
+
     return (
         <div id='about' style={{backgroundImage: `url(${imageBackgroundAbout})`, backgroundSize: 'cover'}} className='w-full mt-0 flex justify-center h-fit py-[5rem] bg-gradient-to-br from-[#505050] via-[#212121] w-full to-[#121212]'>
                 <div className='group text-wrap w-[90%] lg:w-[1280px] h-full flex flex-col text-center gap-2 justify-center align-middle'>
                     <h1 className='text-4xl font-bold text-zinc-50'>About me</h1>
                     <span className='text-md font-regular text-zinc-50'>As a professional fullstack developer, I specialize in using <span className="bg-clip-text text-transparent font-bold bg-gradient-to-r from-[#FDC500] to-[#FFE383]">TypeScript, JavaScript, React.js,</span> and <span className="bg-clip-text text-transparent font-bold bg-gradient-to-r from-[#FDC500] to-[#FFE383]">Node.js</span> to create robust and scalable applications. With a strong foundation built since I began coding in January 2023, I've already developed a range of projects, including an email system, an educational platform like Google Classroom, and multiple RESTful APIs using Express.js. My focus is on delivering high-quality, maintainable code that meets the needs of end-users and stakeholders alike.</span>
+                    <div className='flex flex-col gap-1 mt-5'>
+                            <span className='font-light text-zinc-50'>Get in touch!</span>
+                            <Button title='Click to copy the email' className='w-fit group text-zinc-900 bg-gradient-to-r from-[#FDC500] to-[#FFE383] flex gap-2 m-auto' onClick={handleClickEmail}>
+                                {isClicked ? (
+                                    <>
+                                        <CopyIcon />
+                                        <span className='font-medium'>Copied to the clipboard!</span>
+                                    </>
+                                ) : (
+                                    <>
+                                        <MailIcon />
+                                        <span className='font-medium'>enzorossidaltoe@hotmail.com.br</span>
+                                    </>
+                                )}
+                            </Button>
+                        </div>
                 </div>
         </div >
     );
--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -3,28 +3,27 @@ import me from '/images/me.png';
 import { CircleChevronDown } from 'lucide-react'
 
 const Intro = () => {
+
     return (
-        // <div  id='top' className='w-full flex justify-center'>
-            <div style={{backgroundImage: `url(${imageBackground})`, backgroundSize: 'cover'}} className='w-full h-fit pb-20 pt-[7rem] w-[90%] justify-center grid grid-cols-1'>
-                <div className='space-y-6 md:space-y-0 lg:space-y-0 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 flex flex-col justify-center lg:w-[1280px] lg:m-auto items-center'>
-                    <img className='m-auto w-[80%] lg:w-[80%] col-span-1 md:col-start-2 lg:col-start-2' src={me} alt="me" />
-                    <div className='col-span-1 md:col-start-1 lg:col-start-1 md:row-start-1 space-y-3 lg:row-start-1 h-full md:h-[40%] lg:h-[40%] md:ml-[3rem] lg:ml-[5rem] flex flex-col gap-1 '>
-                        <div className='group text-zinc-50 text-center lg:text-left md:text-left flex flex-col gap-2'>
-                            <p className='text-lg font-regular'>Hi, i'm</p>
-                            <h1 className='text-[7rem] font-extrabold mt-[-2rem] ml-[-.5rem] mb-[-2rem]'>Enzo</h1>
-                            <p className='text-lg font-regular'>Fullstack developer</p>
-                        </div>
-                        <div className='m-auto w-fit bg-[#FDC500] px-4 p-2 rounded-full'>
-                            <span className='text-lg font-medium text-zinc-950'>Welcome to my portfolio</span>
-                        </div>
-                        <div className='flex flex-row gap-2 m-auto w-fit'>
-                            <CircleChevronDown className='text-white' />
-                            <span className='text-sm text-zinc-50'>Scroll down for more</span>
-                        </div>
+        <div style={{ backgroundImage: `url(${imageBackground})`, backgroundSize: 'cover' }} className='w-full h-fit pb-20 pt-[7rem] w-[90%] justify-center grid grid-cols-1'>
+            <div className='space-y-6 md:space-y-0 lg:space-y-0 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 flex flex-col justify-center lg:w-[1280px] lg:m-auto items-center'>
+                <img className='m-auto w-[80%] lg:w-[80%] col-span-1 md:col-start-2 lg:col-start-2' src={me} alt="me" />
+                <div className='col-span-1 md:col-start-1 lg:col-start-1 md:row-start-1 space-y-3 lg:row-start-1 h-full md:h-[40%] lg:h-[40%] md:ml-[3rem] lg:ml-[5rem] flex flex-col gap-1 '>
+                    <div className='group text-zinc-50 text-center lg:text-left md:text-left flex flex-col gap-2'>
+                        <p className='text-lg font-regular'>Hi, i'm</p>
+                        <h1 className='text-[7rem] font-extrabold mt-[-2rem] ml-[-.5rem] mb-[-2rem]'>Enzo</h1>
+                        <p className='text-lg font-regular'>Fullstack developer</p>
+                    </div>
+                    <div className='m-auto w-fit justify-center text-center bg-[#FDC500] flex flex-col gap-2 p-4 rounded-full'>
+                        <span className='text-lg font-medium text-zinc-950'>Welcome to my portfolio</span>
+                    </div>
+                    <div className='flex flex-row gap-2 m-auto w-fit'>
+                        <CircleChevronDown className='text-white' />
+                        <span className='text-sm text-zinc-50'>Scroll down for more</span>
                     </div>
                 </div>
             </div>
-        // </div>
+        </div>
     )
 }
 
